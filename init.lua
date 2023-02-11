2813fd4f0bf8c30f3a8b716821130a3bb7e75358
crespire <hello@crespire.dev>
2023-02-11 14:14:04 -0500

    fix: remove .gitignore and move nvim-tree config to after/

--- init.lua
+++ init.lua
@@ -6,8 +6,5 @@ vim.opt.termguicolors = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- empty setup using defaults
-require("nvim-tree").setup()
-
 -- override ftplugin markdown style
 vim.g.markdown_recommended_style = 0
