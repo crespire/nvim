9957b5ab54dae520bc7be0ab05fee9b155770f31
crespire <hello@crespire.dev>
2023-02-11 12:04:39 -0500

    update: remove harpoon

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -24,7 +24,6 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use('RRethy/nvim-treesitter-endwise')
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -114,11 +114,6 @@ _G.packer_plugins = {
     path = "/home/crespire/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
-  harpoon = {
-    loaded = true,
-    path = "/home/crespire/.local/share/nvim/site/pack/packer/start/harpoon",
-    url = "https://github.com/theprimeagen/harpoon"
-  },
   ["lsp-zero.nvim"] = {
     loaded = true,
     path = "/home/crespire/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
